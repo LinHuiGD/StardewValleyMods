abf0ac04d4df6bf248eb6c6017f4e07634985b7e
LinHuiGD <linhui_gd@hotmail.com>
2025-05-25 15:37:11 +0800

    tabs to spaces

--- WaterDepthOverlay/WaterTileData.cs
+++ WaterDepthOverlay/WaterTileData.cs
@@ -13,9 +13,9 @@ internal class WaterTileData
     public string LayerID;
     public int TileIndex;
     public Texture2D Texture; // water background texture
-	public Rectangle SourceRect; // source rectangle of water background texture
-	public int TileX; // Indicates which water tile this object refers to, required for drawing water animation
-	public int TileY;
+    public Rectangle SourceRect; // source rectangle of water background texture
+    public int TileX; // Indicates which water tile this object refers to, required for drawing water animation
+    public int TileY;
     public WaterTileData(string layerID, Tile tile, int tileX, int tileY)
     {
         LayerID = layerID;
